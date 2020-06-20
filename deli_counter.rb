ff9ac8baf2e2268bc96ca98db00d6b3293c99053
Jose Hidago <jhidalgo521@gmail.com>
2020-06-20 23:34:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,5 @@ end
 
 def take_a_number(arr, name)
   arr << name
-  puts "Welcome, #{name}. You are number #{arr.size} in line."
+  puts "Welcome, #{name}. You are number #{arr.length} in line."
 end
