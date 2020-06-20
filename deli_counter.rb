41ba4c5cfff1d8599e7e64f0881217ce31091aa1
Jose Hidago <jhidalgo521@gmail.com>
2020-06-20 22:57:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 
 def line(arr)
   if arr.size == 0 
-   puts "The line is currently empty."
+    puts "The line is currently empty."
   else
     arr.each.with_index do |n, i|
     puts "This line currently: #{i + 1}. #{n}"
