40b5ee83aa9ca721fb26a9748c5eb0e6ef5a6583
Jose Hidago <jhidalgo521@gmail.com>
2020-06-20 23:46:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(arr, name)
 end
 
 def now_serving(arr)
-  if array.length == 0
+  if arr.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array.shift}."
